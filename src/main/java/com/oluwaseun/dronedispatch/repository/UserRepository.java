36382eba8dd54d91732b31aa516e80bab1ad2323
Oluwaseun Oyinloye <oluwaseun.oyinloye@globalaccelerex.com>
2023-09-14 17:32:45 +0100

    user sign in and sign up, service and controller

--- src/main/java/com/oluwaseun/dronedispatch/repository/UserRepository.java
+++ src/main/java/com/oluwaseun/dronedispatch/repository/UserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Long> {
     AppUser findByUsername(String username);
+
+    boolean existsByUsername(String username);
 }
