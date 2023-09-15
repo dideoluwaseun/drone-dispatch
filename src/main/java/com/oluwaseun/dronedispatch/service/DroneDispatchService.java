9fcc712ed61d25736ee5f8277d33a5746fb5367a
Oluwaseun Oyinloye <oluwaseun.oyinloye@globalaccelerex.com>
2023-09-15 11:07:33 +0100

    update drone state and battery capacity handle http message not readable exception prevent the drone from being in LOADING state if the battery level is below 25%

--- src/main/java/com/oluwaseun/dronedispatch/service/DroneDispatchService.java
+++ src/main/java/com/oluwaseun/dronedispatch/service/DroneDispatchService.java
@@ -3,6 +3,7 @@ package com.oluwaseun.dronedispatch.service;
 import com.oluwaseun.dronedispatch.model.dto.DroneDTO;
 import com.oluwaseun.dronedispatch.model.dto.DroneResponse;
 import com.oluwaseun.dronedispatch.model.dto.LoadDroneRequest;
+import com.oluwaseun.dronedispatch.model.dto.UpdateDroneRequest;
 import com.oluwaseun.dronedispatch.model.entity.Drone;
 import org.springframework.data.domain.Page;
 
@@ -12,4 +13,5 @@ public interface DroneDispatchService {
     DroneResponse getDroneById(Long id);
     void loadDrone(LoadDroneRequest request);
     Page<DroneResponse> getAllAvailableDrones(Integer pageIndex, Integer pageSize);
+    void updateDroneStateAndBatteryCapacity(UpdateDroneRequest request, Long id);
 }
